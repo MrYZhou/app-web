0c2e92c46048a10261359da858a090c38f2e6cac
DESKTOP-S62A507\l'g <1762861715@qq.com>
2022-06-29 23:16:25 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+      dts: "./src/auto-imports.d.ts",
+      imports: ["vue", "pinia", "vue-router", "@vueuse/core"],
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
+    Components({
+      dts: "./src/components.d.ts",
+      // imports 指定组件所在位置，默认为 src/components
+      dirs: ["src/components/"],
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+});
